46d172a3165fb6e38cfe8ae45a0b6b7d149b52ee
Luca Milanesio <luca.milanesio@gmail.com>
2012-11-12 16:45:46 -0800

    Sample plugin for commit message validation.
    Shows how to add additional commit validation to
    Gerrit using a plugin.
    
    Validation can be based on:
    - user performing the Git command
    - Gerrit project
    - any of the commit elements
    - ref-name
    
    Change-Id: Ie283e29feee319fd4f3b7e3f3b833dbe1dbf5c9e
    Signed-off-by: Luca Milanesio <luca.milanesio@gmail.com>
    Signed-off-by: David Pursehouse <david.pursehouse@sonymobile.com>

---
+++ src/main/java/com/googlesource/gerrit/plugins/validators/HelloWorldValidator.java
@@ -0,0 +1,49 @@
+// Copyright (C) 2012 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package com.googlesource.gerrit.plugins.validators;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.gerrit.extensions.annotations.Listen;
+import com.google.gerrit.server.events.CommitReceivedEvent;
+import com.google.gerrit.server.git.validators.CommitValidationException;
+import com.google.gerrit.server.git.validators.CommitValidationListener;
+import com.google.gerrit.server.git.validators.CommitValidationMessage;
+import com.google.inject.Singleton;
+
+@Listen
+@Singleton
+public class HelloWorldValidator implements CommitValidationListener {
+  private static Logger log = LoggerFactory.getLogger(HelloWorldValidator.class);
+  
+  @Override
+  public List<CommitValidationMessage> onCommitReceived(CommitReceivedEvent receiveEvent)
+		  throws CommitValidationException {
+    final String commitMessage = receiveEvent.commit.getFullMessage();
+    final List<CommitValidationMessage> messages = new ArrayList<CommitValidationMessage>();
+    
+    if (commitMessage.indexOf("hello world") < 0) {
+      log.warn("HelloWorldValidator> Commit "
+          + receiveEvent.commit.getId().getName() + " REJECTED");
+      throw new CommitValidationException("Only 'hello world' commit messages are allowed ;-)");
+    }
+    log.info("HelloWorldValidator> Commit "
+        + receiveEvent.commit.getId().getName() + " ACCEPTED");
+    return messages;
+  }
+}
